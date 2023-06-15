9bcaa91d0daa96b47f7383440cb7b68c32d21fd5
Valeriia <indri.hendrix@gmail.com>
2023-06-15 20:51:58 +0300

    [15062203] Add classroom progress

---
+++ src/main/java/org/example/Loop.java
@@ -0,0 +1,99 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Loop {
+
+
+    public static void main(String[] args) {
+        //for; foreach; do while; while;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World!");
+        }
+        String[] fruits = {"apple", "banana", "kiwi", "pear"};
+        //fruits[0] = apple;
+        //fruits[1] = banana;
+        //...
+        for (int i = 0; i < fruits.length; i++) {
+            if (fruits[i].equals("kiwi")) {
+                System.out.println("found it");
+                break;
+            }
+        }
+
+// enhanced for
+        for (String fruit : fruits) {
+            System.out.println("fruit = " + fruit);
+
+        }
+
+        for (int i = 20; i <= 40; i++) {
+            System.out.println(i);
+        }
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+
+            }
+            System.out.println("Sum of even numbers: " + sum);
+        }
+        //6;
+
+        int number = 6;
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i * number + " ");
+        }
+
+        // WHILE LOOP
+        int i = 10;
+        while (i > 0) {
+            System.out.println("Hello world!");
+            i--;
+        }
+        int j = 0;
+        while (j < 10) {
+            System.out.println("Hello World");
+            j++;
+        }
+
+        //DO WHILE
+        int k = 0;
+        do {
+            System.out.println("Hello World");
+            k++;
+        } while (k < 10);
+
+        Scanner myScanner = new Scanner(System.in);
+        Random randomizer = new Random();
+        int randomNumber = randomizer.nextInt( 5 );
+
+
+        boolean isGuessed = false;
+        while (!isGuessed) {
+            System.out.println("Please enter number I guessed:");
+        int inputNumber = myScanner.nextInt();
+        if (randomNumber == inputNumber) {
+            System.out.println("You guessed the number!");
+            IsGuessed = true;
+        } else {
+            System.out.println("Try again!");
+        }
+
+    }
+
+    // TODO: Limit guess with 3 times;
+
+
+        Stream.of(fruits).Filter(s -> s.equals("kiwi").forEach(System.out::println);
+
+        String name = "Nikita";
+        String reversed = "";
+        for (int l = name.length() - 1; l >= 0 ; l--) {
+            reversed = reversed + name.charAt(l);
+
+        }
+        System.out.println(reversed);
+        }
--- src/main/java/org/example/classroom/ArraysIfs.java
+++ src/main/java/org/example/classroom/ArraysIfs.java
@@ -1,172 +1,172 @@
-package org.example.classroom;
-
-import java.util.Arrays;
-
-public class ArraysIfs {
-
-    public static void main(String[] args) {
-
-        // int Array
-
-        int[] numbers = {1, 5, 9, 10, 20, 30, 50};
-        System.out.println(numbers[3]); // print 10
-
-        //numbers[0] = 1
-        //numbers[1] = 5
-        //numbers[2] = 9
-        //numbers[....] = 50
-
-//        System.out.println(numbers[7]); - has exception - out of bounds
-
-        int[] ages = new int[10];
-        ages[0] = 18;
-        ages[3] = 24;
-
-        System.out.println(Arrays.toString(ages));
-        System.out.println(ages);
-
-        // String array
-
-        String[] names = {"John", "Andrew", "Mary", "Angelica"};
-        System.out.println(Arrays.toString(names));
-        System.out.println(names[3]); // print Angelica
-
-        // Logical Statements
-
-        /*
-
-        if(condition) {
-
-        }
-
-        condition = boolean (true or false)
-
-         */
-
-        if (5 < 10) {
-            System.out.println("Yes, this is correct!");
-        }
-
-        boolean isSummer = false;
-        if (isSummer) {
-            System.out.println("Yes, this is summer and its quite hot in here!");
-        }
-
-//    If Statement
-//    Check if a given number is positive or negative. - done
-//    Check if a number is divisible by 2.
-//    Check if a character is a vowel or consonant. (aeiou) / aeiouAEIOU ***
-
-
-        int x = 11;
-
-        if (x > 0) {
-            System.out.println("Number is positive:" + x);
-        }
-
-        if (x < 0) {
-            System.out.println("Number is negative:" + x);
-        }
-
-        if (x % 2 == 0) {
-            System.out.println("This number can be divided: " + x);
-        }
-
-
-        /*
-
-        if(true) {
-        do something if true
-        } else {
-        do something another if false
-        }
-
-       */
-
-        if (x > 0) {
-            System.out.println("Number is positive:" + x);
-        } else {
-            System.out.println("Number is negative: " + x);
-        }
-        // If-Else Statement:
-        // Check if a given number is even or odd.
-        // Check if
-
-        x = 18;
-        // <= 9less or equals)
-        // >= (more or equals)
-        // == (compare)
-
-        if (x < 18) {
-            System.out.println("Person is not eligible for voting");
-        } else {
-            System.out.println("person can go vote");
-
-
-            isSummer = true;
-            boolean isWinter = false;
-            boolean isAutumn = false;
-
-            if (isSummer) {
-                System.out.println("Right, this is summer!");
-            } else if (isWinter) {
-                System.out.println("Right, this is winter!");
-            } else if (isAutumn) {
-                System.out.println("Right, this is autumn!");
-            } else {
-                System.out.println("Not sure, but it can be spring!");
-            }
-
-            int grade = 0;
-
-            if (grade == 1) {
-                System.out.println("Unsatisfactory");
-            } else if (grade == 2) {
-                System.out.println("Unsatisfactory");
-            } else if (grade == 3) {
-                System.out.println("Unsatisfactory");
-            } else if (grade == 4) {
-                System.out.println("Almost satisfactory");
-            } else if (grade == 5) {
-                System.out.println("Almost satisfactory");
-            } else if (grade == 6) {
-                System.out.println("Almost good");
-            } else if (grade == 7) {
-                System.out.println("Good");
-            } else if (grade == 8) {
-                System.out.println("Very good");
-            } else if (grade == 9) {
-                System.out.println("Excellent");
-            } else if (grade == 10) {
-                System.out.println("Perfect");
-            }
-
-            int angle = 90;
-
-            if (angle > 0 && angle < 90) {
-                System.out.println("acute ang.");
-            } else if (angle == 90) {
-                System.out.println("right ang.");
-            } else if (angle > 90 && angle < 180) {
-                System.out.println("obtuse ang.");
-            } else if (angle == 180) {
-                System.out.println("straight");
-            } else {
-                System.out.println("provided angle is not supported");
-            }
-
-            int[] numberArray = {5, 7, 10};
-            int max = 0;
-        }
-        int[] numArray = {4, 18, 5};
-        int MaxNum = numArray[1] < numArray [2]) {
-
-
-
-
-
-            }
-        }
-    }
-}
-
+//package org.example.classroom;
+//
+//import java.util.Arrays;
+//
+//public class ArraysIfs {
+//
+//    public static void main(String[] args) {
+//
+//        // int Array
+//
+//        int[] numbers = {1, 5, 9, 10, 20, 30, 50};
+//        System.out.println(numbers[3]); // print 10
+//
+//        //numbers[0] = 1
+//        //numbers[1] = 5
+//        //numbers[2] = 9
+//        //numbers[....] = 50
+//
+////        System.out.println(numbers[7]); - has exception - out of bounds
+//
+//        int[] ages = new int[10];
+//        ages[0] = 18;
+//        ages[3] = 24;
+//
+//        System.out.println(Arrays.toString(ages));
+//        System.out.println(ages);
+//
+//        // String array
+//
+//        String[] names = {"John", "Andrew", "Mary", "Angelica"};
+//        System.out.println(Arrays.toString(names));
+//        System.out.println(names[3]); // print Angelica
+//
+//        // Logical Statements
+//
+//        /*
+//
+//        if(condition) {
+//
+//        }
+//
+//        condition = boolean (true or false)
+//
+//         */
+//
+//        if (5 < 10) {
+//            System.out.println("Yes, this is correct!");
+//        }
+//
+//        boolean isSummer = false;
+//        if (isSummer) {
+//            System.out.println("Yes, this is summer and its quite hot in here!");
+//        }
+//
+////    If Statement
+////    Check if a given number is positive or negative. - done
+////    Check if a number is divisible by 2.
+////    Check if a character is a vowel or consonant. (aeiou) / aeiouAEIOU ***
+//
+//
+//        int x = 11;
+//
+//        if (x > 0) {
+//            System.out.println("Number is positive:" + x);
+//        }
+//
+//        if (x < 0) {
+//            System.out.println("Number is negative:" + x);
+//        }
+//
+//        if (x % 2 == 0) {
+//            System.out.println("This number can be divided: " + x);
+//        }
+//
+//
+//        /*
+//
+//        if(true) {
+//        do something if true
+//        } else {
+//        do something another if false
+//        }
+//
+//       */
+//
+//        if (x > 0) {
+//            System.out.println("Number is positive:" + x);
+//        } else {
+//            System.out.println("Number is negative: " + x);
+//        }
+//        // If-Else Statement:
+//        // Check if a given number is even or odd.
+//        // Check if
+//
+//        x = 18;
+//        // <= 9less or equals)
+//        // >= (more or equals)
+//        // == (compare)
+//
+//        if (x < 18) {
+//            System.out.println("Person is not eligible for voting");
+//        } else {
+//            System.out.println("person can go vote");
+//
+//
+//            isSummer = true;
+//            boolean isWinter = false;
+//            boolean isAutumn = false;
+//
+//            if (isSummer) {
+//                System.out.println("Right, this is summer!");
+//            } else if (isWinter) {
+//                System.out.println("Right, this is winter!");
+//            } else if (isAutumn) {
+//                System.out.println("Right, this is autumn!");
+//            } else {
+//                System.out.println("Not sure, but it can be spring!");
+//            }
+//
+//            int grade = 0;
+//
+//            if (grade == 1) {
+//                System.out.println("Unsatisfactory");
+//            } else if (grade == 2) {
+//                System.out.println("Unsatisfactory");
+//            } else if (grade == 3) {
+//                System.out.println("Unsatisfactory");
+//            } else if (grade == 4) {
+//                System.out.println("Almost satisfactory");
+//            } else if (grade == 5) {
+//                System.out.println("Almost satisfactory");
+//            } else if (grade == 6) {
+//                System.out.println("Almost good");
+//            } else if (grade == 7) {
+//                System.out.println("Good");
+//            } else if (grade == 8) {
+//                System.out.println("Very good");
+//            } else if (grade == 9) {
+//                System.out.println("Excellent");
+//            } else if (grade == 10) {
+//                System.out.println("Perfect");
+//            }
+//
+//            int angle = 90;
+//
+//            if (angle > 0 && angle < 90) {
+//                System.out.println("acute ang.");
+//            } else if (angle == 90) {
+//                System.out.println("right ang.");
+//            } else if (angle > 90 && angle < 180) {
+//                System.out.println("obtuse ang.");
+//            } else if (angle == 180) {
+//                System.out.println("straight");
+//            } else {
+//                System.out.println("provided angle is not supported");
+//            }
+//
+//            int[] numberArray = {5, 7, 10};
+//            int max = 0;
+//        }
+//        int[] numArray = {4, 18, 5};
+//        int MaxNum = numArray[1] < numArray [2]) {
+//
+//
+//
+//
+//
+//            }
+//        }
+//    }
+//}
+//
